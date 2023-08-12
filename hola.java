d9abe9d861f14423647dc7e1c730d9f93060db9f
pazsanchezs <94194097+pazsanchezs@users.noreply.github.com>
2023-08-12 18:47:02 -0400

    Update hola.java

--- hola.java
+++ hola.java
@@ -1,9 +1,11 @@
 public class HolaMundo {
   /*Alumnas:
             Bethania Chavez
+            Maria Paz Sanchez
   */
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo!");
+        System.out.println("Este es un codigo en java");
     }
 }
 
